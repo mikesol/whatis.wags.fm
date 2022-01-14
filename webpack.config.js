e83c1d4672b252a59d28fe5618afc02025a74a59
Mike Solomon <mike@wavr.so>
2022-01-14 10:13:02 +0200

    removes dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
 module.exports = env => ({
 	mode: "development",
